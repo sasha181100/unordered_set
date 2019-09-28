03d8133b34d18026380629734891e45bddceeb30
sasha181100 <pokamolodoy2000@gmail.com>
2019-09-28 15:35:55 +0300

    release v3

--- unordered_set.h
+++ unordered_set.h
@@ -14,15 +14,15 @@ template<typename T>
 
 struct unordered_set {
 
-    template<typename V>
+    
     struct iterator {
         iterator() = default;
         iterator(iterator const &other) = default;
         iterator& operator=(iterator const &other) = default;
-        V const &operator *() const {
+        T const &operator *() const {
             return table[position_in_table][cell_number];
         }
-        V const *operator ->() const {
+        T const *operator ->() const {
             return &table[position_in_table][cell_number];
         }
         iterator &operator++() {
@@ -41,36 +41,28 @@ struct unordered_set {
             ++(*this);
             return res;
         }
-        friend bool operator==(iterator<V> const &q, iterator<V> const &w) {
+        friend bool operator==(iterator const &q, iterator const &w) {
             return (q.table.begin() == w.table.begin()) && (q.position_in_table == w.position_in_table) && (q.cell_number == w.cell_number);
         }
-        friend bool operator!=(iterator<V> const &q, iterator<V> const &w) {
+        friend bool operator!=(iterator const &q, iterator const &w) {
             return !(q == w);
         }
     private:
-        std::vector<std::vector<V>> const &table;
+        std::vector<std::vector<T>> const &table;
         size_t position_in_table, cell_number;
         friend unordered_set;
-        explicit iterator(std::vector<std::vector<V>> const &a, size_t p, size_t c) :table(a), position_in_table(p), cell_number(c) {
+        explicit iterator(std::vector<std::vector<T>> const &a, size_t const p, size_t const c) :table(a), position_in_table(p), cell_number(c) {
 
         }
     };
-    typedef iterator<T> Iterator;
-    typedef iterator<T const > const_iterator;
-    typedef std::reverse_iterator<Iterator> reverse_iterator;
-    typedef std::reverse_iterator<const_iterator> const_reverse_iterator;
+    //typedef iterator Iterator;
+    typedef iterator const_iterator;
 
     size_t size() {
         return cnt;
     }
 
-    Iterator begin() {
-        for (size_t i = 0; i < hash_table.size(); i++) {
-            if (hash_table.size() != 0) {
-                return Iterator(hash_table, i, 0);
-            }
-        }
-    }
+
     const_iterator begin() const {
         for (size_t i = 0; i < hash_table.size(); i++) {
             if (hash_table.size() != 0) {
@@ -78,47 +70,29 @@ struct unordered_set {
             }
         }
     }
-    Iterator end() {
-        return Iterator(hash_table, hash_table.size(), 0);
-    }
 
     const_iterator end() const {
         return const_iterator(hash_table, hash_table.size(), 0);
     }
 
-    reverse_iterator rbegin() {
-        return reverse_iterator(end());
-    }
-
-    const_reverse_iterator rbegin() const {
-        return const_reverse_iterator(end());
-    }
-
-    reverse_iterator rend() {
-        return reverse_iterator(begin());
-    }
-
-    const_reverse_iterator rend() const {
-        return const_reverse_iterator(begin());
-    }
     unordered_set() : hash_table(8), capacity(8), cnt(0) {
 
     }
     unordered_set(unordered_set const &other) = default;
     unordered_set &operator=(unordered_set const &other) = default;
-    Iterator find(T const&x) {
+    const_iterator find(T const&x) {
         size_t hash = count_hash(x);
         for (size_t i = 0; i < hash_table[hash].size(); i++) {
             if (hash_table[hash][i] == x) {
-                return Iterator(hash_table, hash, i);
+                return const_iterator(hash_table, hash, i);
             }
         }
         //Iterator it = end();
         return end();
     }
-    std::pair<Iterator, bool> insert(T const&x) {
-        Iterator it = find(x);
-        //Iterator it1 = end();
+    std::pair<const_iterator, bool> insert(T const&x) {
+        const_iterator it = find(x);
+        const_iterator it1 = end();
         if (it != end()) {
             return {it, 0};
         }
@@ -128,10 +102,10 @@ struct unordered_set {
         if (hash_table[hash].size() > sqrt(capacity)) {
             enlargement();
         }
-        return {Iterator(hash_table, hash, hash_table[hash].size() - 1), 1};
+        return {const_iterator(hash_table, hash, hash_table[hash].size() - 1), 1};
     }
     void erase(T const &x) {
-        Iterator it = find(x);
+        const_iterator it = find(x);
         if (it != end()) {
             size_t hash = count_hash(x);
             for (size_t i = 0; i < hash_table[hash].size(); i++) {
@@ -151,7 +125,7 @@ private:
     void enlargement() {
         capacity *= 2;
         std::vector<std::vector<T>> tmp(capacity);
-        for (Iterator it = begin(); it != end(); it++) {
+        for (const_iterator it = begin(); it != end(); it++) {
             size_t new_hash = count_hash(*it);
             tmp[new_hash].push_back(*it);
         }
